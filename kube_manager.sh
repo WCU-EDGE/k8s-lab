8e54f3c2606076ee0801d38b08dd91efbf20ac09
Linh B Ngo <lngo@wcupa.edu>
2024-01-18 18:48:06 -0500

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -31,24 +31,12 @@ systemctl start rke2-server.service
 
 cp /var/lib/rancher/rke2/server/node-token /opt/keys/
 
+# Setup kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# this subnet is to support flannel
-# kubeadm init --pod-network-cidr=10.244.0.0/16 > /opt/keys/kube.log
-
-#sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml /local/repository/kube-apiserver.yaml.backup
-#sudo sed -i '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=5000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml
-
-#sleep 90
-
-#sudo touch /opt/keys/kube_done
-
-#while IFS= read -r line; do
-#  mkdir -p /users/$line/.kube
-#  sudo cp -i /etc/kubernetes/admin.conf /users/$line/.kube/config
-#  sudo chown $line: /users/$line/.kube/config
-#done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
-
-# weave is not contactable
-#sudo -H -u $1 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
-#sudo -H -u $1 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+while IFS= read -r line; do
+  mkdir -p /users/$line/.kube
+  sudo cp -i /etc/rancher/rke2/rke2.yaml /users/$line/.kube/config
+  sudo chown $line: /users/$line/.kube/config
+done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
